03d4cafd8f0b8600466de32abd6de633b83a2ea7
박준영 <junsparker@gmail.com>
2022-05-31 10:06:48 +0900

    보고서 작성 모듈 추가
    보고서 작성하는 모듈 result.py 추가함. result.py에 사용 되는 fpdf 모듈 requirements.txt에 추가함.

---
+++ result.py
@@ -0,0 +1,47 @@
+import pandas as pd
+from fpdf import FPDF
+from matplotlib import pyplot as plt
+
+def createpdf(point_csv):
+    # model.modelcon()
+    result_data=pd.read_csv(point_csv, index_col=0)
+    length = len(result_data)/5
+    pdf = FPDF()
+    pdf.set_auto_page_break(auto=False)
+    pdf.add_page(orientation='L')
+    pdf.set_x(0)
+    pdf.set_y(0)
+    pdf.set_margins(0, 0)
+    pdf.set_left_margin(0)
+    pdf.set_font('Arial', 'B', size=20)
+    pdf.set_fill_color(170)
+    pdf.rect(0,0, 80, 250, 'F')
+    pdf.image('logo.png', 10, 10, 60, 60, "PNG")
+
+    pdf.set_x(100)
+    pdf.set_y(0)
+    pdf.set_left_margin(90)
+    # pdf.set_font('Arial', 'B', size=15)
+    pdf.cell(30, 30, txt="1. model and CV Graph", ln=4, align='L')
+    plt.plot(result_data)
+    plt.ylim([0, 120])
+
+    plt.yticks([20, 40, 60, 80, 100])
+    plt.xticks([length, length*2, length*3, length*4])
+    plt.legend(['cv', 'ml'])
+    plt.savefig('data/cv_and_ml.png')
+    pdf.image('data/cv_and_ml.png', 80, 20, 180, 90, "PNG",'data/cv_and_ml.png')
+
+    plt.cla()
+
+    pdf.set_font('Arial', 'B', size=20)
+    pdf.cell(30, 170, txt="2. Result Graph", ln=4, align='L')
+    result_data['ml+cv']=result_data['cv']*0.3+result_data['ml']*0.7
+    plt.plot(result_data['ml+cv'])
+    plt.ylim([0, 120])
+    plt.yticks([20, 40, 60, 80, 100])
+    plt.xticks([length, length*2, length*3, length*4])
+
+    plt.savefig('data/cv+ml.png')
+    pdf.image('data/cv+ml.png', 80, 120, 180, 90, "PNG", 'data/cv+ml.png')
+    pdf.output("ResultReport1.pdf", 'F')
